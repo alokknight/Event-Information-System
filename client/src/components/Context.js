6e973145ec6ab74b9fdf0eec34bc52afe74e37f1
imPGupta <prashantkgupta17@gmail.com>
2022-04-23 13:39:37 +0530

    ubuntu push

---
+++ client/src/components/Context.js
@@ -0,0 +1,4 @@
+import React from 'react'
+export const myContext = React.createContext();
+export const Provider = myContext.Provider;
+export const Consumer = myContext.Consumer;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,10 +6,24 @@ import FAQ from './FAQ';
 import Footer from './Footer';
 import About from './About';
 import Sponsers from './Sponsers';
+import axios from 'axios';
 // import Gallary from './Gallary';
 // import axios from 'axios';
 
 class Home extends React.Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name: '',
+         loggedIn: localStorage.getItem('userTokenTime')
+      }
+    }
+    getUserName=()=>{
+        if(this.state.loggedIn){
+            axios.get('/')
+        }
+    }
     render (){
         return(
             <div>
